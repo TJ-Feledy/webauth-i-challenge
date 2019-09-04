1a9b2a4e9bb821ea59eaee8cea0fe9d3770dd90e
teej387 <teej387@yahoo.com>
2019-09-04 18:08:02 -0500

    Imported middleware into server.js and set root get handler.

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const bcryptjs = require('bcryptjs')
+
+const db = require('./data/dbConfig.js')
 
 const server = express()
 
 server.use(express.json())
 
+server.get('/', (req, res) => {
+  res.send('Server is up and running!')
+})
 
+server.post('/api/register', (req, res) => {
+  let user = req.body
+
+  user.password = bcryptjs.hashSync(user.password, 10)
+
+
+})
 
 module.exports = server
